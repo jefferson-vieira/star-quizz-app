aa3f8ebfd2364401564687541173e7eebac99b41
Jefferson Vieira <jefferson98.vieira@gmail.com>
2019-02-02 00:24:48 -0200

    Timer funcionando

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,1 +1,3 @@
+export const MAX_TIME_IN_SECONDS = 120;
+
 export const API_URL = 'https://swapi.co/api';
--- src/pages/game/GameTimer.jsx
+++ src/pages/game/GameTimer.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
-
 import { Link } from 'react-router-dom';
+
+import moment from 'moment';
 
 import Progress from '../../components/Progress';
 
-import moment from 'moment';
+import { MAX_TIME_IN_SECONDS } from '../../configs';
 
 class GameTimer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      // time: '00:02:00'
-      maxTime: 120,
-      count: 120
+      maxTime: MAX_TIME_IN_SECONDS,
+      time: MAX_TIME_IN_SECONDS
     };
   }
 
@@ -22,36 +22,52 @@ class GameTimer extends Component {
   }
 
   componentWillUnmount() {
-    clearInterval(this.state.interval);
+    this.stopGame();
   }
 
   startTimer = () => {
-    this.setState({ time: moment(0, 'HH:mm:ss').seconds(this.state.count) })
-    // const interval = setInterval(() => {
-    //   this.setState({ time: this.state.time - 1 });
-    // }, 1000);
-    // this.setState({ interval });
+    const interval = setInterval(() => {
+      if (this.state.time <= 0) {
+        this.stopGame();
+      } else {
+        this.setState(prevState => ({ time: prevState.time - 1 }));
+      }
+    }, 1000);
+    this.setState({ interval });
+  };
+
+  getTime = () => {
+    return moment('00:02:00', 'HH:mm:ss')
+      .subtract(this.state.maxTime - this.state.time, 'seconds')
+      .format('HH:mm:ss');
+  };
+
+  getProgress = () => (this.state.time / this.state.maxTime) * 100;
+
+  stopGame = () => {
+    clearInterval(this.state.interval);
+    alert('fim');
   };
 
   render() {
     return (
-      <div className="game-header__timer mt-3">
+      <div className="game-header__timer m-3">
         <div className="row align-items-center border border-warning rounded p-3 m-1">
-          <div className="col-12 timer text-center">
-            <p id="timer" className="text-border">
-              {this.state.time}
+          <div className="col-12 text-center">
+            <p id="timer" className="text-border mb-0">
+              {this.getTime()}
             </p>
           </div>
           <div className="col-12 my-3">
-            <Progress valuenow={this.state.time / (this.state.maxTime / 100)} />
+            <Progress valuenow={this.getProgress()} />
           </div>
-          <div className="col-6 offset-3 text-center">
+          <div className="col-12 text-center">
             <Link
               to="/home"
               className="btn btn-opt btn-danger"
               title="Desistir"
             >
-              Sair
+              Desistir
             </Link>
           </div>
         </div>
